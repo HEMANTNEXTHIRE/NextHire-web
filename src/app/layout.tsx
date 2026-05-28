a36ec78697bcfaa36c81d967acbde5c658419f95
Hemant Kumar <hemant@nexthireconsulting.com>
2026-05-28 13:53:58 +0530

    fix(layout): update Google Tag Manager ID in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -133,7 +133,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MSDC94KK');`}
+          })(window,document,'script','dataLayer','GTM-MW22WQ6Q');`}
         </Script>
 
         {/* Finsweet Cookie Consent */}
@@ -168,7 +168,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* GTM noscript */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MSDC94KK"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MW22WQ6Q"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
